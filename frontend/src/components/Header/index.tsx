55e88ec108ef746966753d98b5307f66bc4d79a4
MarcioSill <marcio.sill@hotmail.com>
2022-09-06 14:01:50 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/Logo.svg'
+import './styles.css'
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src= {logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por
+                    <a href="https://www.instagram.com/stories/marcio.sill/2915920857218927608/"> marcio.sill@hotmail.com</a>
+                </p>
+            </div>
+        </header>
+
+    )
+}
+
+export default Header
